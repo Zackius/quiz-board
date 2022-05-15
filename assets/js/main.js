0f026cb21fdb5a31fb2c8de3cd705aa471bf30df
Zackius <ndunguzachary24@gmail.com>
2022-05-15 21:53:20 +0300

    js updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,8 @@
-var check1 = document.getElementById('q11')
-var check2 = document.getElementById('q12')
-var check3 = document.getElementById('q13')
-var check4 = document.getElementById('q14')
-
-var check5 = document.getElementById('q21')
-var check6= document.getElementById('q22')
-var check7= document.getElementById('q23')
-var check8 = document.getElementById('q24')
-
-var check9 = document.getElementById('q31')
-var check2 = document.getElementById('q32')
-var check3 = document.getElementById('q33')
-var check4 = document.getElementById('q34')
-
-var check1 = document.getElementById('q41')
-var check2 = document.getElementById('q42')
-var check3 = document.getElementById('q43')
-var check4 = document.getElementById('q44')
-
-
-check
-
-
-
-
-
-
-
-
-
+function newName( ){
+    alert("Welcome,  please answer the questions below")
+}
+function checked(){
+    if (){
+  
+    }
+}
