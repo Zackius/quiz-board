8ef8dab41a29fe3f5ca694ccf9ed1ab94686e16e
Zackius <ndunguzachary24@gmail.com>
2022-05-15 20:25:51 +0300

    js updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
+var check1 = document.getElementById('q11')
+var check2 = document.getElementById('q12')
+var check3 = document.getElementById('q13')
+var check4 = document.getElementById('q14')
 
-var button = document.getElementById('btn')
+var check5 = document.getElementById('q21')
+var check6= document.getElementById('q22')
+var check7= document.getElementById('q23')
+var check8 = document.getElementById('q24')
 
-button.addEventListener('click')
+var check9 = document.getElementById('q31')
+var check2 = document.getElementById('q32')
+var check3 = document.getElementById('q33')
+var check4 = document.getElementById('q34')
 
-function button(){
-    alert('Welcome')
-}
-console.log('button()')
+var check1 = document.getElementById('q41')
+var check2 = document.getElementById('q42')
+var check3 = document.getElementById('q43')
+var check4 = document.getElementById('q44')
+
+
+check
+
+
+
+
+
+
+
+
+
