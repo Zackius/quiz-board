7839db467f4a45a1824bd40a39c11fd55e6ea4fb
Zackius <ndunguzachary24@gmail.com>
2022-05-15 22:40:02 +0300

    js logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
+var check = document.getElementsByClassName('answer')
+
 function newName( ){
     alert("Welcome,  please answer the questions below")
 }
+function calc(){
+    if (document.getElementById('forms').checked )
+
+}  
+
 
