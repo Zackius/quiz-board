89c0132b2c11f6149ffe51cd90bdf71592188577
Zackius <ndunguzachary24@gmail.com>
2022-05-15 22:45:50 +0300

    latest

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
-var check = document.getElementsByClassName('answer')
+
 
 function newName( ){
     alert("Welcome,  please answer the questions below")
 }
-function calc(){
-    if (document.getElementById('forms').checked )
-
-}  
 
 
