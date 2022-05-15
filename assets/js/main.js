c133e5a58f04de30b25e12d36070017f5be1ac30
Zackius <ndunguzachary24@gmail.com>
2022-05-15 23:30:18 +0300

    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,17 @@
 function newName( ){
     alert("Welcome,  please answer the questions below")
 }
+ 
+
+var tick = document.getElementsByName('marks') 
+
+marks = 20;
+
+function check(){
+    if(document.getElementById('form').checked)
+    
+} marks + 0{
+    else (marks * 0)
+}
 
 
