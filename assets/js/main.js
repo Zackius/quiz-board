46bd1d0db53050f521ffeb0fd6d5e8490a365a63
Zackius <ndunguzachary24@gmail.com>
2022-05-16 18:24:14 +0300

    js modiification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
-var tick= document.getElementById('forms')
-var submitButton = document.getElementById('subbtn')
+var check1 = document.getElementById('from1')
+var check2 = document.getElementById('from2')
+var check3 = document.getElementById('from3')
+var check4 = document.getElementById('from4')
 
-submitButton.addEventListener(click){
-    if(tick.checked)
-} alert('')
+var cheked = ['check1', 'check2' ,'check3', 'check4']
 
-{
+function ticked(){
+    if(cheked[].checked)
+    alert('on to the next')
+    {
 
+    }
 }
-
