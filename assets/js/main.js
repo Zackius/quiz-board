169f8da8b9d5312d5f51670f87de804bcf4ca139
Zackius <ndunguzachary24@gmail.com>
2022-05-19 02:55:37 +0300

    latest js updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
-var check1 = document.getElementById('from1')
-var check2 = document.getElementById('from2')
-var check3 = document.getElementById('from3')
-var check4 = document.getElementById('from4')
+var firstQuestion = document.getElementById('forms1')
+var secondQuestion = document.getElementById('forms2')
+var thirdQuestion = document.getElementById('forms3')
+var fourthQuestion = document.getElementById('forms4')
 
-var cheked = ['check1', 'check2' ,'check3', 'check4']
 
-function ticked(){
-    if(cheked[].checked)
-    alert('on to the next')
-    {
 
-    }
+function  checkUp() {
+    var answer = 0; 
+    var calc = forms1.value;
+   if(calc == 'A programming language that makes websites interactive' ) {answer ++} 
+
+
+   var calc = forms2.value;
+   if (calc == 'JS') { answer ++ }
+
+
+   var calc = forms3.value;
+   if (calc == 'A JS container for stroring data') { answer ++ }
+
+
+   var calc = forms4.value;
+   if (calc == 'Strings, Boolean, numbers, undefined,  and null ') {answer++    
+   }
+   document.write(answer)
 }
